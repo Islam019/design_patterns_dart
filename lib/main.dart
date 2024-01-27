e81885aac8c774e0305dc4c5596eff2fd24c2036
Eslam Osama <islamusama56@gmail.com>
2024-01-27 05:04:21 +0200

    add & use Equatable package instead of using normal operator overriding

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,43 @@
+import 'package:equatable/equatable.dart';
+
 void main() {
   // CreationalPatterns.singletonPattern();
   // CreationalPatterns.prototypePattern();
 
-  const test1 = TestEquality(1);
-  const test2 = TestEquality(1);
+  const test1 = TestEqualityWithEquatable(100, 6, 5, 8);
+  const test2 = TestEqualityWithEquatable(101, 6, 5, 8);
+
+  print(test1.hashCode);
+  print(test2.hashCode);
 
   if (test1 == test2) {
     print('Equal');
   }
 }
 
-class TestEquality {
+class TestEqualityWithoutEquatable {
   final int x;
-  const TestEquality(this.x);
+  final int y;
+  final int z;
+  final int id;
+  const TestEqualityWithoutEquatable(this.id, this.x, this.y, this.z);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TestEqualityWithoutEquatable && hashCode == other.hashCode;
+
+  @override
+  int get hashCode => Object.hash(id, x, y, z);
+}
+
+class TestEqualityWithEquatable extends Equatable {
+  final int x;
+  final int y;
+  final int z;
+  final int id;
+  const TestEqualityWithEquatable(this.id, this.x, this.y, this.z);
+
+  @override
+  List<Object?> get props => [id, x, y, z];
 }
